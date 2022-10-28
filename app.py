dfa6d14686bfac601a9d3c88859ec3b6182e6613
Mahadev Tamhanekar <78201917+mahadev2001@users.noreply.github.com>
2022-10-28 19:08:46 +0530

    Update app.py

--- app.py
+++ app.py
@@ -14,13 +14,16 @@ def home():
 
 @app.route('/predict',methods=['POST','GET'])
 def predict():
-    if request.method == 'GET:
+    if request.method == 'GET':
         return render_template('index.html',pred='Sutaible career option for this student is {}'.format(predclassprob))
-    if request.method == 'POST:
+    if request.method == 'POST':
+        for x in request.form.values():
+            print(x)
+            print("value")
         userdata=[int(x) for x in request.form.values()]
         final=[np.array(userdata)]
-        print(clf.predict(userdata)) 
-        classprobs = clf.predict_proba(userdata)
+        print(model.predict(userdata)) 
+        classprobs = model.predict_proba(userdata)
         predclassprob = np.max(classprobs)
 
         return render_template('result.html',result=predclassprob)
